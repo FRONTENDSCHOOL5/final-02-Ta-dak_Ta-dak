06e9e7ef986e324b2e879284c78b9a5550a42c30
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-06-08 20:34:52 +0900

    :art: Design: 로그인 버튼,  팔로우 버튼, reset 스타일, 공통 스타일 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react'
+
+import { LoginButton, FollowButton, FollowButtonSm } from './components/common/Button'
+import { ResetStyle } from './style/ResetStyle'
+import {GlobalStyle} from './style/GlobalStyle'
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
-  );
+    <>
+        <ResetStyle />
+        <GlobalStyle />
+        <LoginButton contents={'로그인'}/>
+        <FollowButton contents={'팔로우'}/>
+        <FollowButtonSm contents={'팔로우'}/>
+    </>
+
+  )
 }
-export default App;
+
+export default App
+
+
---
+++ my-app/src/style/GlobalStyle.jsx
@@ -0,0 +1,31 @@
+const { createGlobalStyle } = require('styled-components');
+
+export const GlobalStyle = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    /* line-height: 1.5; */
+  }
+
+  :root {
+    --background-color: #FCFBF3;
+    --first-feed-btn-color: #FDF5B9;
+    --basic-btn-color: #B9D6A3;
+    --profile-btn-color: #4E683F;
+    --input-active-btn-color: #1C8A00;
+    --disabled-font-color: #A2A378;
+    --disabled-btn-color: #E6E2E2;
+    
+    --font--size-lg: 16px;
+    --font--size-md: 14px;
+    --font--size-sm: 12px;
+
+    --font--Bold: 700;
+    --font--Medium: 500;
+    --font--Regular: 400;
+  }
+`;
+
+export default GlobalStyle;
