060a626680487632262b3142b720826f13c3767e
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-06-12 00:52:18 +0900

    :art: 버튼 컴포넌트 수정, 컴포넌트 테스트파일 생성 후 분리 (#6)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react'
-
-import { LoginButton, FollowButton, FollowButtonSm } from './components/common/Button'
-import { ResetStyle } from './style/ResetStyle'
-import {GlobalStyle} from './style/GlobalStyle'
-import { NavBar } from './components/common/NavBar'
-import InputTest from './componentsTest/InputTest'
+import ComponentsTest from './ComponentsTest'
 
 function App() {
   return (
     <>
-        <ResetStyle />
-        <GlobalStyle />
-        <LoginButton contents={'로그인'}/>
-        <FollowButton contents={'팔로우'}/>
-        <FollowButtonSm contents={'팔로우'}/>
-        <NavBar />
-        <InputTest />
+      <ComponentsTest/>
     </>
-
   )
 }
 
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,50 +1,157 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-export function LoginButton ({contents}) {
-  return (
-    // contents : 로그인, 다음, 감귤마켓시작하기
-  <LoginButtonStyle>
-  {contents}
-  </LoginButtonStyle> 
-  )
+export function GreenLgBtn({ contents }) {
+  return <GreenLgBtnStyle>{contents}</GreenLgBtnStyle>
 }
 
-export function FollowButton({contents}) {
-  return (
-    <FollowButtonStyle>
-    {contents}
-    </FollowButtonStyle>
-  )
+export function GreenMdBtn({ contents }) {
+  return <GreenMdBtnStyle>{contents}</GreenMdBtnStyle>;
 }
 
-export function FollowButtonSm({contents}) {
-  return (
-    <FollowButtonSmStyle>
-    {contents}
-    </FollowButtonSmStyle>
-  )
+export function GreenMsBtn({ contents }) {
+  return <GreenMsBtnStyle>{contents}</GreenMsBtnStyle>;
 }
 
-const LoginButtonStyle = styled.button`
+export function GreenSmBtn({ contents }) {
+  return <GreenSmBtnStyle>{contents}</GreenSmBtnStyle>;
+}
+
+export function BoldLgBtn({ contents }) {
+  return <BoldLgBtnStyle>{contents}</BoldLgBtnStyle>;
+}
+
+export function BoldMdBtn({ contents }) {
+  return <BoldMdBtnStyle>{contents}</BoldMdBtnStyle>;
+}
+
+export function BoldMsBtn({ contents }) {
+  return <BoldMsBtnStyle>{contents}</BoldMsBtnStyle>;
+}
+
+export function BoldSmBtn({ contents }) {
+  return <BoldSmBtnStyle>{contents}</BoldSmBtnStyle>;
+}
+
+export function WhiteLgBtn({ contents }) {
+  return <WhiteLgBtnStyle>{contents}</WhiteLgBtnStyle>;
+}
+
+export function WhiteMdBtn({ contents }) {
+  return <WhiteMdBtnStyle>{contents}</WhiteMdBtnStyle>;
+}
+
+export function WhiteMsBtn({ contents }) {
+  return <WhiteMsBtnStyle>{contents}</WhiteMsBtnStyle>;
+}
+
+export function WhiteSmBtn({ contents }) {
+  return <WhiteSmBtnStyle>{contents}</WhiteSmBtnStyle>;
+}
+
+export function GreyLgBtn({ contents }) {
+  return <GreyLgBtnStyle>{contents}</GreyLgBtnStyle>;
+}
+
+export function GreyMdBtn({ contents }) {
+  return <GreyMdBtnStyle>{contents}</GreyMdBtnStyle>;
+}
+
+export function GreyMsBtn({ contents }) {
+  return <GreyMsBtnStyle>{contents}</GreyMsBtnStyle>;
+}
+
+export function GreySmBtn({ contents }) {
+  return <GreySmBtnStyle>{contents}</GreySmBtnStyle>;
+}
+
+const GreenLgBtnStyle = styled.button`
   width: 322px;
   height: 44px;
-  border-radius: 44px;
   font-size: var(--font--size-md);
   background-color: var(--basic-color-2);
-`
+  border-radius: 44px;
+`;
 
-const FollowButtonStyle = styled.button`
+const GreenMdBtnStyle = styled.button`
   width: 120px;
   height: 34px;
   font-size: var(--font--size-md);
-  background-color: var(--basic-color-2); 
+  background-color: var(--basic-color-2);
   border-radius: 30px;
 `;
 
-const FollowButtonSmStyle = styled.button`
+const GreenMsBtnStyle = styled.button`
+  width: 90px;
+  height: 32px;
+  font-size: var(--font--size-md);
+  background-color: var(--basic-color-2);
+  border-radius: 30px;
+`;
+
+const GreenSmBtnStyle = styled.button`
   width: 56px;
   height: 28px;
   font-size: var(--font--size-sm);
-  background-color: var(--basic-color-2); 
+  background-color: var(--basic-color-2);
   border-radius: 30px;
+`;
+
+const BoldLgBtnStyle = styled(GreenLgBtnStyle)`
+  background-color: var(--basic-color-3);
+  color : #FCFBF3;
+`;
+
+const BoldMdBtnStyle = styled(GreenMdBtnStyle)`
+  background-color: var(--basic-color-3);
+  color : #FCFBF3;
+`;
+
+const BoldMsBtnStyle = styled(GreenMsBtnStyle)`
+  background-color: var(--basic-color-3);
+  color : #FCFBF3;
+`;
+
+const BoldSmBtnStyle = styled(GreenSmBtnStyle)`
+  background-color: var(--basic-color-3);
+  color : #FCFBF3;
+`;
+
+const WhiteLgBtnStyle = styled(GreenLgBtnStyle)`
+  background-color: #fff;
+  color: var(--basic-color-7);
+`;
+
+const WhiteMdBtnStyle = styled(GreenMdBtnStyle)`
+  background-color: #fff;
+  color: var(--basic-color-7);
+`;
+
+const WhiteMsBtnStyle = styled(GreenMsBtnStyle)`
+  background-color: #fff;
+  color: var(--basic-color-7);
+`;
+
+const WhiteSmBtnStyle = styled(GreenSmBtnStyle)`
+  background-color: #fff;
+  color: var(--basic-color-7);
+`;
+
+const GreyLgBtnStyle = styled(GreenLgBtnStyle)`
+  background-color: var(--basic-color-6);
+  color: var(--basic-color-5);
+`;
+
+const GreyMdBtnStyle = styled(GreenMdBtnStyle)`
+  background-color: var(--basic-color-6);
+  color: var(--basic-color-5);
+`;
+
+const GreyMsBtnStyle = styled(GreenMsBtnStyle)`
+  background-color: var(--basic-color-6);
+  color: var(--basic-color-5);
+`;
+
+const GreySmBtnStyle = styled(GreenSmBtnStyle)`
+  background-color: var(--basic-color-6);
+  color: var(--basic-color-5);
 `;
