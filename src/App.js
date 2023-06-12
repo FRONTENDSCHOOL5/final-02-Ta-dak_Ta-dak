c89f5286d51cc9986fa3a6e0cc4e385171ba59a1
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-06-12 10:47:41 +0900

    :lipstick: Style: App.js 줄바꿈

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import ComponentsTest from './ComponentsTest'
+import { LoginButton, FollowButton, FollowButtonSm } from './components/common/Button'
+import { ResetStyle } from './style/ResetStyle'
+import {GlobalStyle} from './style/GlobalStyle'
+import { NavBar } from './components/common/NavBar'
+import InputTest from './componentsTest/InputTest'
+import UserPostList from './components/UserPostList/UserPostList'
+import { Profile } from './components/common/Profile'
+import {Modal} from './components/common/Modal'
+import {Alert} from './components/common/Alert'
+
 
 function App() {
   return (
     <>
-      <ComponentsTest/>
+        <ResetStyle />
+        <GlobalStyle />
+        <LoginButton contents={'로그인'}/>
+        <FollowButton contents={'팔로우'}/>
+        <FollowButtonSm contents={'팔로우'}/>
+        <NavBar />
+        <InputTest />
+        <Profile />
+        <Modal />
+        <Alert />
+        <UserPostList />
     </>
+
   )
 }
 
 export default App
-
 
