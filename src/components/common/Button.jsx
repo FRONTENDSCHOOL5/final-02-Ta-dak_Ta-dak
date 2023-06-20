f13dcf6292853bc2b8db6868bc6f10de1e094b66
Eunnnnnnnn <0624pjh@gmail.com>
2023-06-20 15:02:26 +0900

    :art: Design: 버튼 디자인 추가 (#57)

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -16,6 +16,10 @@ export function GreenSmBtn({ contents, handleFunc }) {
   return <GreenSmBtnStyle onClick={handleFunc}>{contents}</GreenSmBtnStyle>;
 }
 
+export function GreenBackBtn({ contents, handleFunc }) {
+  return <GreenBackBtnStyle onClick={handleFunc}>{contents}</GreenBackBtnStyle>;
+}
+
 export function BoldLgBtn({ contents, handleFunc }) {
   return <BoldLgBtnStyle onClick={handleFunc}>{contents}</BoldLgBtnStyle>;
 }
@@ -106,6 +110,14 @@ const GreenSmBtnStyle = styled.button`
   width: 56px;
   height: 28px;
   font-size: var(--font--size-sm);
+  background-color: var(--basic-color-2);
+  border-radius: 30px;
+`;
+
+const GreenBackBtnStyle = styled.button`
+  width: 120px;
+  height: 44px;
+  font-size: var(--font--size-md);
   background-color: var(--basic-color-2);
   border-radius: 30px;
 `;
