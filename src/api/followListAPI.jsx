e82686ebea84fc2a610e085f4c27ee970a57b714
JoWonYeong <wswy17@gmail.com>
2023-06-21 10:42:49 +0900

    :bulb: Feat: 팔로워 팔로잉 불러오는 api 수정 (#67)

--- src/api/followListAPI.jsx
+++ src/api/followListAPI.jsx
@@ -1,9 +1,7 @@
 import { axiosAuth } from './settingAxios';
 
-export const getFollowingList = async (accountname) => {
-  const reqUrl = `/profile/${accountname}/following`;
-  // paging limit skip
-  // GET /profile/:accountname/following?limit=Number&skip=Number
+export const getFollowingList = async (accountname, loadFollowSeq) => {
+  const reqUrl = `/profile/${accountname}/following?limit=20&skip=${loadFollowSeq}`;
 
   try {
     const response = await axiosAuth.get(reqUrl);
@@ -14,11 +12,9 @@ export const getFollowingList = async (accountname) => {
   }
 };
 
-export const getFollowerList = async (accountname) => {
-  const reqUrl = `/profile/${accountname}/follower`;
-  // paging limit skip
-  // GET /profile/:accountname/follower/?limit=Number&skip=Number
-  
+export const getFollowerList = async (accountname, loadFollowSeq) => {
+  const reqUrl = `/profile/${accountname}/follower/?limit=20&skip=${loadFollowSeq}`;
+
   try {
     const response = await axiosAuth.get(reqUrl);
     return response.data;
