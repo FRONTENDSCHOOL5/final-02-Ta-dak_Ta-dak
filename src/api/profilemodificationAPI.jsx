d17cd9d55aab8b54980fde415dbb620150034695
JiEun <0624pjh@gmail.com>
2023-06-22 14:49:42 +0900

    :bulb: Feat: 프로필 수정 기능 구현 (#63)

--- src/api/profilemodificationAPI.jsx
+++ src/api/profilemodificationAPI.jsx
@@ -1,43 +1,43 @@
-export default async function putProfilemodification(username,id, intro) {
-  const response = await fetch(`https://api.mandarin.weniv.co.kr/user`, {
-    method: 'PUT',
-    headers: {
-        'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGIyNWM1YjJjYjIwNTY2MzM2MTkzZSIsImV4cCI6MTY5MjAyNDgzNiwiaWF0IjoxNjg2ODQwODM2fQ.q04djYvmJA49rPOTmIUsEigdnYK-HHLTqhpaLBKXf4I',
-        'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-      'user':{
-          'username': username,
-          'accountname': id,
-          'intro': intro,
-          'image': ''
-      },
-    }),
-  });
+// export default async function putProfilemodification(username,id, intro) {
+//   const response = await fetch(`https://api.mandarin.weniv.co.kr/user`, {
+//     method: 'PUT',
+//     headers: {
+//         'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OGIyNWM1YjJjYjIwNTY2MzM2MTkzZSIsImV4cCI6MTY5MjAyNDgzNiwiaWF0IjoxNjg2ODQwODM2fQ.q04djYvmJA49rPOTmIUsEigdnYK-HHLTqhpaLBKXf4I',
+//         'Content-type' : 'application/json'
+//     },
+//     body: JSON.stringify({
+//       'user':{
+//           'username': username,
+//           'accountname': id,
+//           'intro': intro,
+//           'image': ''
+//       },
+//     }),
+//   });
 
-  const json = await response.json();
+//   const json = await response.json();
 
-  return json;
-}
+//   return json;
+// }
 
 //axios
-// import { axiosAuth } from "./settingAxios";
+import { axiosAuth } from "./settingAxios";
 
-// export const profilemodificationReq = async (username, id, intro, image) => {
-//   const reqUrl = '/user/profilemodicationn';
-//   const body = {
-//     user: {
-//       username,
-//       id,
-//       intro,
-//       image
-//     }
-//   };
-//   try {
-//     const response = await axiosAuth.put(reqUrl, body)
-//     return response.data
-//   } catch (error) {
-//     console.error('Request error', error)
-//     throw error
-//   }
-// }
+export const profilemodificationReq = async (username, accountname, intro, image) => {
+  const reqUrl = '/user';
+  const body = {
+    user: {
+      username,
+      accountname,
+      intro,
+      image
+    }
+  };
+  try {
+    const response = await axiosAuth.put(reqUrl, body)
+    return response.data
+  } catch (error) {
+    console.error('Request error', error)
+    throw error
+  }
+}
--- src/pages/ProfileModificationPage.jsx
+++ src/pages/ProfileModificationPage.jsx
@@ -1,32 +1,64 @@
 import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import useImageUploader from '../hooks/useImageUploader'
 import styled from 'styled-components';
 
 import UploadHeader from '../components/header/UploadHeader';
 import { Input } from '../components/common/Input';
 import { FileUploadLg } from '../components/common/FileUpload'
-import putProfilemodification from '../api/profilemodificationAPI';
+import { profilemodificationReq } from '../api/profilemodificationAPI';
+import { postAccountValid } from '../api/signupAPI';
+
 
 export default function ProfileModificationPage() {
-  const [name, setName] = useState('')
-  const handleNameInput = (event) => {
-    setName(event.target.value);
-  };
 
-  const [id, setId] = useState('')
+  const { handleImageChange, imageURL, imagePath } = useImageUploader();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userInfo = location.state;
+  const [name, setName] = useState(userInfo.username || '')
+  const [nameValid, setNameValid] = useState(true);
+  const [nameAlertMsg, setNameAlertMsg] = useState('')
+  const [id, setId] = useState(userInfo.accountname || '')
+  const [idValid, setIdValid] = useState(true);
+  const [idAlertMsg, setIdAlertMsg] = useState('');
+  const [intro, setIntro] = useState(userInfo.intro || '')
+  
+  const handleNameInput = (event) => {
+    const value = event.target.value;
+    if (value.length >= 2 && value.length <= 10) {
+      setNameAlertMsg('')
+      setNameValid(true)
+    } else {
+      setNameAlertMsg('2~10자 이내여야 합니다.')
+      setNameValid(false)
+    }
+    setName(value)
+  };
+  
+  const handleIdProfile = async () => {
+    const pattern = /^[A-Za-z0-9_.]+$/;
+    if(pattern.test(id)) {
+      const Msg = await postAccountValid(id)
+      setIdAlertMsg(Msg)
+      Msg === "사용 가능한 계정ID 입니다." ?  setIdValid(true) :  setIdValid(false)
+    } else {
+      setIdAlertMsg('*영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.');
+      setIdValid(false)
+    }
+  }
+
   const handleIdInput = (event) => {
     setId(event.target.value);
   };
   
-  const [intro, setIntro] = useState('')
   const handleIntroInput = (event) => {
     setIntro(event.target.value);
   };
 
-  const { handleImageChange, imageURL, imagePath } = useImageUploader();
-
   const submitModification = async () => {
-    const data = await putProfilemodification(name, id, intro);
+    const data = await profilemodificationReq(name || userInfo.username, id || userInfo.accountname, intro || userInfo.intro, imagePath || userInfo.image);
+    navigate(`/profile/${id || userInfo.accountname}`);
     console.log(data.user);
   }
 
@@ -35,13 +67,13 @@ export default function ProfileModificationPage() {
       <ProfileModificationStyle>
         <UploadHeader valid={true} contents={'저장'} handleUploadBtnClick={submitModification} />
         <FileUploadStyle>
-          <FileUploadLg onChange={handleImageChange} /> 
+          <FileUploadLg onChange={handleImageChange} url={imageURL || userInfo.image} id={'profileEdit'}/> 
           {/*onChange={handleImageChange} url={imageURL}  */} 
         </FileUploadStyle>
         <div className='profileInfo'>
-          <Input id={'user-name'} type={'text'} label={'사용자 이름'} placeholder='2~10자 이내여야 합니다.' value={name} onChange={handleNameInput}/>
-          <Input id={'user-id'} type={'text'} label={'계정 ID'} placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.' value={id} onChange={handleIdInput}/>
-          <Input id={'user-intro'} type={'text'} label={'소개'} placeholder='자신과 판매할 상품에 대해 소개해 주세요!' value={intro} onChange={handleIntroInput}/>
+          <Input id={'user-name'} type={'text'} label={'사용자 이름'} placeholder={'2~10자 이내여야 합니다.'} value={name} onChange={handleNameInput} valid={nameValid} alertMsg={nameAlertMsg}/>
+          <Input id={'user-id'} type={'text'} label={'계정 ID'} placeholder={'영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'} value={id} onChange={handleIdInput} valid={idValid} onBlur={handleIdProfile} alertMsg={idAlertMsg}/>
+          <Input id={'user-intro'} type={'text'} label={'소개'} placeholder={'자신과 판매할 상품에 대해 소개해 주세요!'} value={intro} onChange={handleIntroInput} />
         </div>
       </ProfileModificationStyle>
     </>
