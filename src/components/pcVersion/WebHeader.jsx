d3dd3722206666b0866613ed4134553ef9f34a06
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-08-16 17:33:39 +0900

    :art: Design: PC 버전 - 배경색상/다크모드 버튼색상/로그아웃 버튼크기 변경 (#118)

--- src/components/pcVersion/WebHeader.jsx
+++ src/components/pcVersion/WebHeader.jsx
@@ -1,35 +1,63 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
-
-import { ReactComponent as Tadak} from '../../assets/img/tadak.svg';
-import { ReactComponent as WebLogo} from '../../assets/img/weblogo.svg';
-import { ReactComponent as IconLogout}  from '../../assets/img/icon-logout.svg';
+import { useSetRecoilState } from 'recoil'
+import { IsLogin, UserAtom } from '../../recoil/AtomUserState';
+import { ReactComponent as Tadak } from '../../assets/img/tadak.svg';
+import { ReactComponent as WebLogo } from '../../assets/img/weblogo.svg';
+import { ReactComponent as IconLogout } from '../../assets/img/icon-logout.svg';
 import DarkModeBtn from '../DarkModeBtn';
 
-export default function WebHeader({handleFunc}) {
+export default function WebHeader() {
+
+  const setUserValue = useSetRecoilState(UserAtom);
+  const setIsLogin = useSetRecoilState(IsLogin);
+  const navigate = useNavigate();
+
+  const location = useLocation();
+  const hideHeaderPaths = [
+    '/splash',
+    '/login',
+    '/signup'
+  ]
+
+  const hideHeader = hideHeaderPaths.includes(location.pathname);
+
+  const handleLogout = (event) =>{
+      setUserValue({})
+      setIsLogin(false)
+      sessionStorage.removeItem('user')
+      navigate('/splash');
+  }
+
   return (
-    <WebHeaderStyle>
-        <WebLogoStyle>
+    <>
+      {!hideHeader &&
+        <WebHeaderStyle>
+          <WebLogoStyle>
             <WebLogo></WebLogo>
             <TadakStyle>
-              <Tadak style={{ width: '104px', height: '25.58 px' }}/>
+              <Tadak style={{ width: '104px', height: '25.58 px' }} />
             </TadakStyle>
-        </WebLogoStyle>
-        <BtnStyle>
-          <DarkModeBtn style={{ width: '44px', height: '44px'}}/>
-          <IconLogout onClick={handleFunc} style={{ width: '30px', height: '30px', cursor: 'pointer', margin: '25px 0'}}/>
-        </BtnStyle>
-    </WebHeaderStyle>
+          </WebLogoStyle>
+          <BtnStyle>
+            <DarkModeBtn style={{ width: '44px', height: '44px' }} />
+            <IconLogout onClick={handleLogout} style={{ width: '23px', height: '23px', cursor: 'pointer'}} />
+          </BtnStyle>
+        </WebHeaderStyle>
+      }
+    </>
   )
 }
 
 const WebHeaderStyle = styled.div`
+    position: relative;
     width: 100vw;
     height: 80px;
-    background-color: #B9D6A3;
+    background-color: var(--basic-color-1);
     display: flex;
     justify-content: space-between;
+    z-index: 1000;
 `;
 
 const WebLogoStyle = styled.div`
@@ -45,7 +73,8 @@ top: -10px;
 `;
 
 const BtnStyle = styled.div`
- display: flex;
- gap: 28px;
- margin-right: 10%;
+  display: flex;
+  align-items: center;
+  gap: 28px;
+  margin-right: 10%;
 `;
