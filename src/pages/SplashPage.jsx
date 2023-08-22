52ca4e2d7cf819eb8c0aa38abd946c29d8bb8b99
jiwon6635 <piok66372@naver.com>
2023-08-22 21:38:54 +0900

    🧸 Chore: 업로드 파일 수정(#123)

--- src/pages/SplashPage.jsx
+++ src/pages/SplashPage.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
-
 import styled, {keyframes} from 'styled-components'
-
 import { ReactComponent as Tadak} from '../assets/img/tadak.svg';
 import { ReactComponent as Fire} from '../assets/img/fire.svg';
 import { ReactComponent as WoodFire} from '../assets/img/woodfire.svg';
 import { ReactComponent as SubTitle} from '../assets/img/subtitle.svg';
 import { ReactComponent as FireworkR} from '../assets/img/firework-r.svg';
 import { ReactComponent as FireworkL} from '../assets/img/firework-l.svg';
-import { ReactComponent as Kakao}  from '../assets/img/kakao-login.svg';
-import { ReactComponent as Google} from '../assets/img/google-login.svg';
-import { ReactComponent as Facebook} from '../assets/img/fb-login.svg';
+import { ReactComponent as Kakao}  from '../assets/img/icon-kakao.svg';
+import { ReactComponent as Google} from '../assets/img/icon-google.svg';
+import { ReactComponent as Facebook} from '../assets/img/icon-facebook.svg';
 
 export default function SplashPage() {
   const navigate = useNavigate();
-
   return (
     <>
     <SplashPageStyle>
@@ -30,21 +27,24 @@ export default function SplashPage() {
         <SubTitleStyle></SubTitleStyle>
       </SplashCharacterContainer>
         <section>
-        {/* <LoginModalStyle>
+        <LoginModalStyle>
           <ul>
             <li>
-              <BtnSocialStyle aria-label='카카오로 로그인'>
+              <BtnSocialStyle className='kakao' aria-label='카카오로 로그인'>
                 <Kakao></Kakao>
+                <p>카카오톡 계정으로 로그인</p>
               </BtnSocialStyle>
             </li>
             <li>
-              <BtnSocialStyle aria-label='구글로 로그인'>
+              <BtnSocialStyle className='google' aria-label='구글로 로그인'>
                 <Google></Google>
+                <p>구글 계정으로 로그인</p>
               </BtnSocialStyle>
             </li>
             <li>
-              <BtnSocialStyle aria-label='페이스북으로 로그인'>
+              <BtnSocialStyle className='facebook' aria-label='페이스북으로 로그인'>
                 <Facebook></Facebook>
+                <p>페이스북 계정으로 로그인</p>
               </BtnSocialStyle>
             </li>
         </ul>
@@ -57,7 +57,7 @@ export default function SplashPage() {
               <button className='Signup'>회원가입</button>
             </Link>
         </div>
-        </LoginModalStyle> */}
+        </LoginModalStyle>
         </section>
     </SplashPageStyle>
     </>
@@ -65,8 +65,7 @@ export default function SplashPage() {
 }
 
 const SplashCharacterContainer = styled.div`
-  position: relative;
-  /* text-align: center;  */
+  text-align: center; 
 `;
 
 const firework1 = keyframes`
@@ -87,7 +86,6 @@ const firework1 = keyframes`
     transform: rotate(8deg);
   }
   `;
-
 const firework2 = keyframes`
   0% {
   opacity: 1;
@@ -98,7 +96,6 @@ const firework2 = keyframes`
   transform: translateY(-80px);
 }
 `
-
 const firework3 = keyframes`
   0% {
   opacity: 1;
@@ -117,7 +114,6 @@ const loginmodal = keyframes`
   40% {
     opacity: 0;
     transform: translateY(0px);
-
 }
   100% {
   opacity: 1;
@@ -127,7 +123,7 @@ const loginmodal = keyframes`
 
 const SplashPageStyle = styled.div `
 background-color: #FFFFEA;
-width: 100%;
+width: var(--basic-width);
 height: var(--basic-height);
   section {
     display: flex;
@@ -135,58 +131,69 @@ height: var(--basic-height);
     position: relative;
     top: -150px
   }
+  @media (min-width: 390px) {
+    width: 100%;
+    height: var(--basic-height);
+    }  
 `
 
 const TitleStyle1 = styled(Tadak)`
-  position: absolute;
-  /* margin: 10vh 96px 0px 50px; */
-` 
-
+  margin: 10vh 96px 0px 50px;
+`
 
 const TitleStyle2 = styled(Tadak)`
-  position: absolute;
-
-  /* margin: 10px 50px 0px 96px; */
+  margin: 10px 50px 0px 96px;
 `
 
 const FireStyle = styled(Fire)`
   margin: 0 auto;
-  position: absolute;
+  position: relative;
   top: 80px;
-  left: 0px;
+  left: -20px;
   z-index: 100;
   animation: ${firework1} 2s infinite;
+  @media (min-width: 768px) {
+    left: -10px;
+    }  
 `  
 
 const FireworkRStyle = styled(FireworkR)`
-  position: absolute;
-  left: 300px;
+  position: relative;
+  left: 240px;
   top: -30px;
   animation: ${firework2} 2s infinite;
+  @media (min-width: 768px) {
+    left: -50px;
+    top: 150px;
+    }  
 `
 
 const FireworkLStyle = styled(FireworkL)`
-  position: absolute;
+  position: relative;
   top: -40px;
-  left: 50px;
+  left: -30px;
   animation: ${firework3} 2s infinite;
+  @media (min-width: 768px) {
+    left: -350px;
+    top: 160px;
+    }  
 ` 
 
 const WoodFireStyle = styled(WoodFire)`
-  position: absolute;
+  position: relative;
   width: 400px;
   height: 400px;
   top: -190px;
 `
 
 const SubTitleStyle = styled(SubTitle)`
-  position: absolute;
+  position: relative;
   text-align: center;
   top: -20px 
 `
 
 const LoginModalStyle = styled.article`
-width: 100%;
+width: var(--basic-width);
 height: 100vh;
 border-radius: 20px;
 background-color: #ffffff;
@@ -199,7 +206,6 @@ animation: ${loginmodal} 3.6s forwards;
     position: relative;
     top: 20px
   }
-
   div {
     margin-top: 50px;
     display: flex;
@@ -211,13 +217,40 @@ animation: ${loginmodal} 3.6s forwards;
         color: var(--common-text-color-2);
       }
   }
+  @media (min-width: 390px) {
+    width: 100%;
+    }
 `
 const BtnSocialStyle = styled.button`
   display: flex;
   align-items: center;
-  justify-content: center;
+  gap: 50px;
   width: 322px;
+  height: 44px;
   margin: 20px auto;
   position: relative;
   border-radius: 44px;
-`
+  p {
+    font-size: var(--font--size-md);
+  }
+  svg {
+    margin-left: 14px;
+  }
+  &.kakao {
+    border: solid 1px #F2C94C;
+  }
+  &.google {
+    border: solid 1px #767676;
+  }
+  &.facebook {
+    border: solid 1px #2D9CDB;
+  }
+  @media (min-width: 768px) {
+    width: calc(100% - 40px);
+    justify-content: center;
+    gap: 10px;
+    
+    svg {
+      margin-left: 0px;
+    }
+  }`
